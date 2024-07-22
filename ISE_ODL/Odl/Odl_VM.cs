e5ba9cb78e6c083dba16cc323e30185495bda774
Matteo Veronesi <veronesimatteo2007@gmail.com>
2024-07-22 12:12:45 +0200

    Aggiungere i file di progetto.

---
+++ ISE_ODL/Odl/Odl_VM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISE_ODL.Odl
+{
+    internal class Odl_VM
+    {
+        public string ID { get; set; }
+    }
+}
