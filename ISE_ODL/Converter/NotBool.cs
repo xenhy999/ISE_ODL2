43590ebe360d8cbf6f0cd4cf646e199ae4784c1c
Matteo Veronesi <veronesimatteo2007@gmail.com>
2024-07-26 15:26:05 +0200

    stile, icona e grafica

---
+++ ISE_ODL/Converter/NotBool.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ISE_ODL.Converter
+{
+    public class NotBool : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool r=(bool)value;
+            return !r;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
