ed70cac5ba3867d741b7afbe029c76f9e4e69680
Matteo Veronesi <veronesimatteo2007@gmail.com>
2024-07-26 18:05:41 +0200

    oggi

---
+++ ISE_ODL/Menu/Colori.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ISE_ODL.Menu
+{
+    public class Colori:BaseBinding
+    {
+        BrushConverter brushConverter = new BrushConverter();
+        public Colori()
+        {
+            Palette.Add((SolidColorBrush)brushConverter.ConvertFrom("#1B262C"));
+            Palette.Add((SolidColorBrush)brushConverter.ConvertFrom("#0F4C75"));
+            Palette.Add((SolidColorBrush)brushConverter.ConvertFrom("#3282B8"));
+            Palette.Add((SolidColorBrush)brushConverter.ConvertFrom("#BBE1FA"));
+        }
+
+        public List<SolidColorBrush> Palette { get; set; } = new List<SolidColorBrush>();
+
+    }
+}
--- ISE_ODL/Menu/MenuPrincipale_V.xaml.cs
+++ ISE_ODL/Menu/MenuPrincipale_V.xaml.cs
@@ -1,16 +1,14 @@
 ﻿using System.IO;
+using System.Text.Json;
 using System.Windows;
 using ISE_ODL.Odl;
-using System.Text.Json;
 namespace ISE_ODL
 {
     public partial class MenuPrincipale_V : Window
     {
         public static readonly string fileName = @$"{AppDomain.CurrentDomain.BaseDirectory}\Commisioni.json";
-
         public MenuPrincipale_V() => InitializeComponent();
         private void Button_Click(object sender, RoutedEventArgs e) => Close();
-
         private void Window_Closed(object sender, EventArgs e)
         {
             List<Odl_M> OdlDaSerializzare = ObjContainer.MenuPrincipale_VM.Commisioni.Where(c => c is Odl_VM).Cast<Odl_VM>().Select(cvm => cvm.Model).ToList();
@@ -18,7 +16,6 @@ namespace ISE_ODL
             foreach (Odl_M o in OdlDaSerializzare) odl.Add(JsonSerializer.Serialize(o));
             File.WriteAllLines(fileName, odl);
         }
-
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             RichiestaEliminazioneOdl_V richiestaEliminazioneOdl_V = new RichiestaEliminazioneOdl_V();
