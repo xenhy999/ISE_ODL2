be6327e2adaaeb5a9d04220c3aedfa2e11835a72
Matteo Veronesi <veronesimatteo2007@gmail.com>
2024-08-02 15:22:03 +0200

    controllo orari

--- ISE_ODL/Intervallo/ModificaIntervallo.cs
+++ ISE_ODL/Intervallo/ModificaIntervallo.cs
@@ -10,17 +10,15 @@ namespace ISE_ODL.Intervallo
     public class ModificaIntervallo : BaseCommand
     {
         public BaseOdl_VM OdlDellIntervallo { get; set; }
-        public Intervallo_M IntervalloDaModificare
-        {
-            get; set;
-        }
+        public Intervallo_M IntervalloDaModificare {get; set;}
         public override void Execute(object parameter)
         {
             string caso = (string)parameter;
             switch (caso)
             {
                 case "ai":
-                    IntervalloDaModificare.OrarioInizio += ObjContainer.Menuprincipale_VM.Settings_VM.IntervalloInModifica;
+                    if (IntervalloDaModificare.OrarioFine-IntervalloDaModificare.OrarioInizio >= ObjContainer.Menuprincipale_VM.Settings_VM.IntervalloInModifica) 
+                        IntervalloDaModificare.OrarioInizio += ObjContainer.Menuprincipale_VM.Settings_VM.IntervalloInModifica;
                     break;
                 case "ri":
                     IntervalloDaModificare.OrarioInizio -= ObjContainer.Menuprincipale_VM.Settings_VM.IntervalloInModifica;
@@ -29,12 +27,12 @@ namespace ISE_ODL.Intervallo
                     IntervalloDaModificare.OrarioFine += ObjContainer.Menuprincipale_VM.Settings_VM.IntervalloInModifica;
                     break;
                 case "rf":
-                    IntervalloDaModificare.OrarioFine -= ObjContainer.Menuprincipale_VM.Settings_VM.IntervalloInModifica;
+                    if (IntervalloDaModificare.OrarioFine - IntervalloDaModificare.OrarioInizio >= ObjContainer.Menuprincipale_VM.Settings_VM.IntervalloInModifica)
+                        IntervalloDaModificare.OrarioFine -= ObjContainer.Menuprincipale_VM.Settings_VM.IntervalloInModifica;
                     break;
             }
             OdlDellIntervallo.OnPropertyChanged(nameof(OdlDellIntervallo.Intervalli));
             OdlDellIntervallo.OnPropertyChanged(nameof(OdlDellIntervallo.Durate));
-
         }
     }
 }
