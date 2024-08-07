819c5b612786954b397a1ea9989cc137456aa9fe
Matteo Veronesi <veronesimatteo2007@gmail.com>
2024-08-07 11:18:02 +0200

    documentazioni durate

--- ISE_ODL/Intervallo/Durata/Durata_F.cs
+++ ISE_ODL/Intervallo/Durata/Durata_F.cs
@@ -1,7 +1,17 @@
 ﻿namespace ISE_ODL.Intervallo.Durata
 {
+    /// <summary>
+    /// La classe statica Durata_F fornisce un metodo per creare istanze di Durata_VM.
+    /// Si tratta della factory di Durata_VM
+    /// </summary>
     public static class Durata_F
     {
+        /// <summary>
+        /// Crea una nuova istanza di Durata_VM con la data e le ore specificate.
+        /// </summary>
+        /// <param name="data">La data associata alla durata.</param>
+        /// <param name="ore">Il periodo di tempo associato alla durata.</param>
+        /// <returns>Restituisce una nuova istanza di Durata_VM con i valori specificati.</returns>
         public static Durata_VM Create(DateOnly data, TimeSpan ore) => new() { Data = data, Ore = ore };
     }
 }
--- ISE_ODL/Intervallo/Durata/Durata_VM.cs
+++ ISE_ODL/Intervallo/Durata/Durata_VM.cs
@@ -1,9 +1,22 @@
 ﻿namespace ISE_ODL.Intervallo.Durata
 {
+    /// <summary>
+    /// La classe Durata_VM rappresenta un modello di visualizzazione (ViewModel) per la durata,
+    /// derivata dalla classe BaseBinding.
+    /// </summary>
     public class Durata_VM : BaseBinding
     {
+        /// <summary>
+        /// Proprietà che indica il periodo di tempo associato alla durata.
+        /// </summary>
         public TimeSpan Ore { get; set; }
+        /// <summary>
+        /// Proprietà che indica la data associata alla durata.
+        /// </summary>
         public DateOnly Data { get; set; }
+        /// <summary>
+        /// Proprietà che indica se la durata è stata scaricata.
+        /// </summary>
         public bool Scaricato { get; set; }
     }
 }
