1e8dc559cc0fe8ae857f2a7fb0d4db024eb5ac01
Matteo Veronesi <veronesimatteo2007@gmail.com>
2024-08-07 13:05:38 +0200

    documentazione giorno

--- ISE_ODL/Resoconto/Giorno/Giorno_F.cs
+++ ISE_ODL/Resoconto/Giorno/Giorno_F.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ISE_ODL.Odl;
-using Windows.UI.WebUI;
-
-namespace ISE_ODL.Resoconto.Giorno
+﻿namespace ISE_ODL.Resoconto.Giorno
 {
-    internal class Giorno_F
+    /// <summary>
+    /// La classe è una factory per la creazione di istanze di Giorno_VM.
+    /// </summary>
+    public class Giorno_F
     {
+        /// <summary>
+        /// Crea una nuova istanza dGiorno_VM utilizzando la data specificata.
+        /// </summary>
+        /// <param name="date">La data da utilizzare per la creazione della vista del giorno.</param>
+        /// <returns>Una nuova istanza diGiorno_VM.</returns>
         public static Giorno_VM Create(DateOnly date) => new Giorno_VM(date);
     }
 }
--- ISE_ODL/Resoconto/Giorno/Giorno_VM.cs
+++ ISE_ODL/Resoconto/Giorno/Giorno_VM.cs
@@ -1,29 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ISE_ODL.Odl;
-
+﻿using ISE_ODL.Odl;
 namespace ISE_ODL.Resoconto.Giorno
 {
-    public class Giorno_VM:BaseBinding
+    /// <summary>
+    /// La classe rappresenta una ViewModel per una data specifica.
+    /// </summary>
+    public class Giorno_VM : BaseBinding
     {
         private DateOnly data;
+        /// <summary>
+        /// Inizializza una nuova istanza di Giorno_VM con la data specificata.
+        /// </summary>
+        /// <param name="data">La data associata a questa vista del giorno.</param>
         public Giorno_VM(DateOnly data) => Data = data;
+        /// <summary>
+        /// Proprietà che indica la data per questa vista del giorno.
+        /// </summary>
+        /// <value>
+        /// La data associata a questa vista del giorno.
+        /// </value>
         public DateOnly Data
         {
             get => data;
             set
             {
                 data = value;
-                if(data==DateOnly.FromDateTime(DateTime.Now)) DataOdierna=true;
-                else DataOdierna=false;
+                if (data == DateOnly.FromDateTime(DateTime.Now)) DataOdierna = true;
+                else DataOdierna = false;
                 OnPropertyChanged(nameof(DataOdierna));
             }
         }
+        /// <summary>
+        /// Proprietà che indica un dizionario che associa un'istanza di Odl_VM a un TimeSpan che rappresenta il tempo lavorato.
+        /// </summary>
+        /// <value>
+        /// Un dizionario in cui la chiave è un'istanza di Odl_VM e il valore è un TimeSpan che rappresenta il tempo lavorato per quel ODL.
+        /// </value>
         public Dictionary<Odl_VM, TimeSpan> OdlLavorati { get; set; } = [];
+        /// <summary>
+        /// Proprietà che indica se la data è odierna.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> se la data è la data odierna; in caso contrario, <c>false</c>.
+        /// </value>
         public bool DataOdierna { get; set; }
     }
 }
