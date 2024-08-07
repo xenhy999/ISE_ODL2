6b18e8ee179bdbe7e1ab1957f357f145497370d9
Matteo Veronesi <veronesimatteo2007@gmail.com>
2024-08-07 13:13:53 +0200

    documentazione settings

--- ISE_ODL/Settings/Setting_F.cs
+++ ISE_ODL/Settings/Setting_F.cs
@@ -1,7 +1,16 @@
 ﻿namespace ISE_ODL.Settings
 {
-    internal class Setting_F
+    /// <summary>
+    /// La classe fornisce metodi statici per la creazione di istanze di Settings_VM.
+    /// </summary>
+    public class Setting_F
     {
+        /// <summary>
+        /// Crea una nuova istanza di Settings_VM.
+        /// </summary>
+        /// <returns>
+        /// Una nuova istanza di Settings_VM.
+        /// </returns>
         static public Settings_VM Create() => new();
     }
 }
--- ISE_ODL/Settings/Settings_VM.cs
+++ ISE_ODL/Settings/Settings_VM.cs
@@ -1,8 +1,20 @@
 ﻿namespace ISE_ODL.Settings
 {
+    /// <summary>
+    /// La classe gestisce le impostazioni dell'applicazione.
+    /// </summary>
     public class Settings_VM : BaseBinding
     {
+        /// <summary>
+        /// Proprietà che indica se il timer è abilitato.
+        /// Il valore è memorizzato nelle impostazioni dell'applicazione.
+        /// </summary>
         public bool TimerAbilitato { get => Properties.Settings.Default.TimerAttivo; set => Properties.Settings.Default["TimerAttivo"] = value; }
+        /// <summary>
+        /// Proprietà che indica la durata del timer.
+        /// Il valore è memorizzato nelle impostazioni dell'applicazione.
+        /// Quando il valore viene impostato, viene notificato il cambiamento e il timer viene ripristinato.
+        /// </summary>
         public TimeSpan DurataDelTimer
         {
             get => Properties.Settings.Default.DurataDelTimer;
@@ -13,6 +25,10 @@
                 ObjContainer.OdlTimer?.ResetTimer();
             }
         }
+        /// <summary>
+        /// Proprietà che indica la durata minima di un ordine di lavoro (ODL).
+        /// Il valore è memorizzato nelle impostazioni dell'applicazione.
+        /// </summary>
         public TimeSpan DurataMinimaOdl
         {
             get => Properties.Settings.Default.DurataMinimaOdl;
@@ -22,6 +38,10 @@
                 Properties.Settings.Default["DurataMinimaOdl"] = value;
             }
         }
+        /// <summary>
+        /// Proprietà che indica l'intervallo di modifica per l'ODL.
+        /// Il valore è memorizzato nelle impostazioni dell'applicazione.
+        /// </summary>
         public TimeSpan IntervalloInModifica
         {
             get => Properties.Settings.Default.IntervalloInModifica;
